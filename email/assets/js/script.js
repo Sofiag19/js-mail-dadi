bcfb0aead22e17a843d3e4ea28e6e1bad85946c0
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-21 17:22:14 +0200

    altre prove

--- email/assets/js/script.js
+++ email/assets/js/script.js
@@ -9,25 +9,25 @@ emailReq = prompt("Inserisci la tua e-mail");
 listaEmail = ["ciao@gmail.com", "bau@gmail.com" , "miao@gmail.com" , "bee@gmail.com" , "mu@gmail.com"];
 
 for (var i = 0; i < listaEmail.length; i++) {
-  if (emailReq == listaEmail[i]) {
-    invitato = true;
+  if (emailReq === listaEmail[i]) {
+    // invitato = true;
     // console.log("benvenuto");
-    // document.getElementById("messaggio").innerHTML = "benvenuto";
+    document.getElementById("messaggio").innerHTML = "benvenuto";
   }else {
-    invitato = false;
+    // invitato = false;
     // console.log("non sei benvenuto");
-    // document.getElementById("messaggio").innerHTML = "non sei benvenuto";
+    document.getElementById("messaggio").innerHTML = "non sei benvenuto";
   }
 }
 //
 // var invitato = emailReq == listaEmail[i];
 //
 // var noninvitato = !(emailReq == listaEmail[i]);
-
-if (invitato = true) {
-  document.getElementById("messaggio").innerHTML = "benvenuto";
-  console.log("benvenuto");
-} else {
-  document.getElementById("messaggio").innerHTML = "non sei benvenuto";
-  console.log("non sei benvenuto");
-}
+//
+// if (invitato = true) {
+//   document.getElementById("messaggio").innerHTML = "benvenuto";
+//   console.log("benvenuto");
+// } else {
+//   document.getElementById("messaggio").innerHTML = "non sei benvenuto";
+//   console.log("non sei benvenuto");
+// }
