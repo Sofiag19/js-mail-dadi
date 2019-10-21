bb73faf8fdca65cfce341447ead1d2fcd1808ecd
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-21 23:04:45 +0200

    ok

--- dadi/assets/js/script.js
+++ dadi/assets/js/script.js
@@ -1,22 +1,23 @@
 // Gioco dei dadi, chi fa di più vince;
 
 // lancio gicatore
-var dadoUtente = Math.floor(Math.random() * 6);
+var dadoUtente = Math.floor((Math.random() * 6)+1);
 
 // lancio pc
-var dadoPc = Math.floor(Math.random() * 6);
+var dadoPc = Math.floor((Math.random() * 6)+1);
 
 // output
 var mess = "Il tuo punteggio è " + dadoUtente + " mentre il pc ha totalizzato " + dadoPc;
+var scrittaId = document.getElementById('scritta');
 
 // comparazione
 if (dadoUtente > dadoPc) {
-  alert (mess + " : Complimenti! Hai vinto!" );
+  scrittaId.innerHTML = mess + " : Complimenti! Hai vinto!"
   console.log(mess + " : Complimenti! Hai vinto!");
 }else if (dadoUtente < dadoPc) {
-  alert (mess + " : Mi dispiace! Hai perso!" );
+  scrittaId.innerHTML = mess + " : Mi dispiace! Hai perso!";
   console.log(mess + " : Mi dispiace! Hai perso!");
 }else {
-  alert ("Avete totalizzato lo stesso punteggio! Riprova se vuoi battere il pc.");
+  scrittaId.innerHTML = "Avete totalizzato lo stesso punteggio! Aggiorna per battere il pc.";
   console.log("Avete totalizzato lo stesso punteggio! Riprova se vuoi battere il pc.");
 }
