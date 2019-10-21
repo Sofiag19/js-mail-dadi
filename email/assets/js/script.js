f85f7153a3272ccf6c11765b6926a6e675f68c2f
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-21 17:13:40 +0200

    non ci siamo

--- email/assets/js/script.js
+++ email/assets/js/script.js
@@ -2,16 +2,34 @@
 // controlla che sia nella lista di chi può accedere e
 // stampa un messaggio appropriato;
 
-var emailReq, listaEmail;
+var emailReq, listaEmail, invitato;
 
 emailReq = prompt("Inserisci la tua e-mail");
 
 listaEmail = ["ciao@gmail.com", "bau@gmail.com" , "miao@gmail.com" , "bee@gmail.com" , "mu@gmail.com"];
 
+
+
 for (var i = 0; i < listaEmail.length; i++) {
-  if (emailReq === listaEmail[i]) {
-    console.log("benvenuto");
+  if (emailReq == listaEmail[i]) {
+    invitato = true;
+    // console.log("benvenuto");
+    // document.getElementById("messaggio").innerHTML = "benvenuto";
   }else {
-    console.log("non sei benvenuto");
+    invitato = false;
+    // console.log("non sei benvenuto");
+    // document.getElementById("messaggio").innerHTML = "non sei benvenuto";
   }
+}
+//
+// var invitato = emailReq == listaEmail[i];
+//
+// var noninvitato = !(emailReq == listaEmail[i]);
+
+if (invitato = true) {
+  document.getElementById("messaggio").innerHTML = "benvenuto";
+  console.log("benvenuto");
+} else if (invitato = false) {
+  document.getElementById("messaggio").innerHTML = "non sei benvenuto";
+  console.log("non sei benvenuto");
 }
