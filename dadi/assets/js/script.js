c50bf8649e4799264b3f330631f5b3a9e6bd1075
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-23 11:25:01 +0200

    eliminazione commenti superflui e aggiunta commenti per descrizioni

--- dadi/assets/js/script.js
+++ dadi/assets/js/script.js
@@ -6,24 +6,29 @@ var dadoUtente = Math.floor((Math.random() * 6)+1);
 // lancio pc
 var dadoPc = Math.floor((Math.random() * 6)+1);
 
-// output
+// output-assegnazione variabili
 var mess = "Il tuo punteggio è " + dadoUtente + " mentre il pc ha totalizzato " + dadoPc;
 var scrittaId = document.getElementById('scritta');
 var puntegUt = document.getElementById('animut');
 var puntegPc = document.getElementById('animpc');
 
-// comparazione
+// comparazione-messaggio
+var mess = "Il tuo punteggio è " + dadoUtente + " mentre il pc ha totalizzato " + dadoPc;
+var scrittaId = document.getElementById('scritta');
+var puntegUt = document.getElementById('animut');
+var puntegPc = document.getElementById('animpc');
+
 if (dadoUtente > dadoPc) {
-  scrittaId.innerHTML = mess + " : Complimenti! HAI VINTO!"
+  scrittaId.innerHTML = mess + " : Complimenti! <br>HAI VINTO!"
   console.log(mess + " : Complimenti! Hai vinto!");
 }else if (dadoUtente < dadoPc) {
-  scrittaId.innerHTML = mess + " : Mi dispiace! HAI PERSO!";
+  scrittaId.innerHTML = mess + " : Mi dispiace! <br>HAI PERSO!";
   console.log(mess + " : Mi dispiace! Hai perso!");
 }else {
-  scrittaId.innerHTML = "Avete totalizzato lo stesso punteggio! Clicca su GIOCA per battere il pc.";
+  scrittaId.innerHTML = "Avete totalizzato lo stesso punteggio! <br>Clicca su GIOCA per battere il pc.";
   console.log("Avete totalizzato lo stesso punteggio! Riprova se vuoi battere il pc.");
 }
-
+// comparazione-immagini
 var valDadoUtente = [1,2,3,4,5,6];
 var valDadoPc = [1,2,3,4,5,6];
 var imgDado = ["<img src='assets/img/uno.jpg' alt='uno'>","<img src='assets/img/due.jpg' alt='due'>","<img src='assets/img/tre.jpg' alt='tre'>","<img src='assets/img/quattro.jpg' alt='quattro'>","<img src='assets/img/cinque.jpg' alt='cinque'>","<img src='assets/img/sei.jpg' alt='sei'>"]
@@ -40,9 +45,6 @@ for (var a = 0; a < valDadoPc.length; a++) {
   }
 }
 
-
-
-// imgDado[dadoUtente -1]
 // if (dadoUtente == 1) {
 //   puntegUt.innerHTML = "<img src='assets/img/uno.jpg' alt='uno'>";
 // }else if (dadoUtente == 2) {
@@ -56,8 +58,6 @@ for (var a = 0; a < valDadoPc.length; a++) {
 // }else if (dadoUtente == 6) {
 //   puntegUt.innerHTML = "<img src='assets/img/sei.jpg' alt='sei'>";
 // }
-//
-//
 //
 // if (dadoPc == 1) {
 //   puntegPc.innerHTML = "<img src='assets/img/uno.jpg' alt='uno'>";
